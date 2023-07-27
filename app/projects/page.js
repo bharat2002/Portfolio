960b8627d8549a109844392032c25f0f8cb3a03b
bharat2002 <bpaliwal70@gmail.come>
2023-07-27 17:48:14 +0530

    projectcards

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,70 +1,66 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import vidweb from '/public/vidwebpng.png'
 
+const projects =[
+  {
+    id:1,
+    title:"Youtube Clone",
+    description:"1Youtube Clone",
+    img: '/public/vidwebpng.png',
+  },
+  {
+    id:1,
+    title:"Youtube Clone",
+    description:"2Youtube Clone",
+    img: '/public/vidwebpng.png',
+  }
+  ,
+  {
+    id:1,
+    title:"Youtube Clone",
+    description:"Youtube Clone",
+    img: '/public/vidwebpng.png',
+  },
+  {
+    id:1,
+    title:"Youtube Clone",
+    description:"Youtube Clone",
+    img: '/public/vidwebpng.png',
+  },
+  {
+    id:1,
+    title:"Youtube Clone",
+    description:"Youtube Clone",
+    img: '/public/vidwebpng.png',
+  },
+]
+  
+
+
 const page = () => {
   return (
+    
+
     <div className='my-14 w-6xl flex flex-wrap flex-row justify-center'>
-        <div className=' m-6 p-4 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
-        <div>
-        <Image height={500} width={500} src={vidweb}/>
+
+    {
+      projects.map((project) => (
+        <div  className=' m-6 p-4 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
+        <div key={project.key}>
+        <Image height={500} width={500} src={require('/public/vidwebpng.png')}/>
         </div>
-        <div className='overflow-y-scroll h-3/5 scrollbar-thin scrollbar-thumb-blue-400 scrollbar-track-gray-300 flex flex-col text-hereis p-4'>
-          <p className='text-center font-bold text-purple-600 text-2xl'>Youtube Clone</p>
-          <p className='text-justify text-md'>This is currently an ongoing project. It is a full stack project with both frontend and backend integration which is fully responsive. responsive React JS application consisting of stunning video sections, custom categories, channel pages, and, most importantly, you can play videos straight from your YouTube Clone App.</p>
+        <div className='overflow-y-scroll h-3/5 scrollbar-thin scrollbar-rounded-full scrollbar-thumb-purple-600  scrollbar-track-gray-300 flex flex-col text-hereis pr-2 py-4'>
+          <p className='text-center font-bold text-purple-600 text-2xl'>{project.title}</p>
+          <p className='text-justify text-md'>{project.description}</p>
         </div>
         </div>
 
-        <div className='p-4 m-6 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
-        <div>
-        <Image height={500} width={500} src={vidweb}/>
-        </div>
-        <div className='overflow-y-scroll h-3/5 scrollbar-thin scrollbar-thumb-blue-400 scrollbar-track-gray-300 flex flex-col text-hereis p-4'>
-          <p className='text-center font-bold text-purple-600 text-2xl'>Youtube Clone</p>
-          <p className='text-justify text-md'>This is currently an ongoing project. It is a full stack project with both frontend and backend integration which is fully responsive. responsive React JS application consisting of stunning video sections, custom categories, channel pages, and, most importantly, you can play videos straight from your YouTube Clone App.</p>
-        </div>
+      ))
+     }
         </div>
 
-        <div className='p-4 m-6 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
-        <div>
-        <Image height={500} width={500} src={vidweb}/>
-        </div>
-        <div className='overflow-y-scroll h-3/5 scrollbar-thin scrollbar-thumb-blue-400 scrollbar-track-gray-300 flex flex-col text-hereis p-4'>
-          <p className='text-center font-bold text-purple-600 text-2xl'>Youtube Clone</p>
-          <p className='text-justify text-md'>This is currently an ongoing project. It is a full stack project with both frontend and backend integration which is fully responsive. responsive React JS application consisting of stunning video sections, custom categories, channel pages, and, most importantly, you can play videos straight from your YouTube Clone App.</p>
-        </div>
-        </div>
-
-        <div className='p-4 m-6 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
-        <div>
-        <Image height={500} width={500} src={vidweb}/>
-        </div>
-        <div className='overflow-y-scroll h-3/5 scrollbar-thin scrollbar-thumb-blue-400 scrollbar-track-gray-300 flex flex-col text-hereis p-4'>
-          <p className='text-center font-bold text-purple-600 text-2xl'>Youtube Clone</p>
-          <p className='text-justify text-md'>This is currently an ongoing project. It is a full stack project with both frontend and backend integration which is fully responsive. responsive React JS application consisting of stunning video sections, custom categories, channel pages, and, most importantly, you can play videos straight from your YouTube Clone App.</p>
-        </div>
-        </div>
-
-        <div className='p-4 m-6 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
-        <div>
-        <Image height={500} width={500} src={vidweb}/>
-        </div>
-        <div className='overflow-y-scroll h-3/5 scrollbar-thin scrollbar-thumb-blue-400 scrollbar-track-gray-300 flex flex-col text-hereis p-4'>
-          <p className='text-center font-bold text-purple-600 text-2xl'>Youtube Clone</p>
-          <p className='text-justify text-md'>This is currently an ongoing project. It is a full stack project with both frontend and backend integration which is fully responsive. responsive React JS application consisting of stunning video sections, custom categories, channel pages, and, most importantly, you can play videos straight from your YouTube Clone App.</p>
-        </div>
-        </div>
-
-        <div className='p-4 m-6 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
-        <div>
-        <Image height={500} width={500} src={vidweb}/>
-        </div>
-        <div className='overflow-y-scroll h-3/5 scrollbar-thin scrollbar-thumb-blue-400 scrollbar-track-gray-300 flex flex-col text-hereis p-4'>
-          <p className='text-center font-bold text-purple-600 text-2xl'>Youtube Clone</p>
-          <p className='text-justify text-md'>This is currently an ongoing project. It is a full stack project with both frontend and backend integration which is fully responsive. responsive React JS application consisting of stunning video sections, custom categories, channel pages, and, most importantly, you can play videos straight from your YouTube Clone App.</p>
-        </div>
-        </div>
-    </div>
   )
 }
 
