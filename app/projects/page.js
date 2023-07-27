f21901429d373698e05293fee308eb05c72cf32d
bharat2002 <bpaliwal70@gmail.come>
2023-07-27 17:58:25 +0530

    projectcar

--- app/projects/page.js
+++ app/projects/page.js
@@ -5,36 +5,25 @@ import vidweb from '/public/vidwebpng.png'
 
 const projects =[
   {
-    id:1,
+    id:"Youtube3",
     title:"Youtube Clone",
     description:"1Youtube Clone",
     img: '/public/vidwebpng.png',
   },
   {
-    id:1,
+    id:"Youtube2",
     title:"Youtube Clone",
     description:"2Youtube Clone",
     img: '/public/vidwebpng.png',
   }
   ,
   {
-    id:1,
+    id:'Youtube1',
     title:"Youtube Clone",
     description:"Youtube Clone",
     img: '/public/vidwebpng.png',
   },
-  {
-    id:1,
-    title:"Youtube Clone",
-    description:"Youtube Clone",
-    img: '/public/vidwebpng.png',
-  },
-  {
-    id:1,
-    title:"Youtube Clone",
-    description:"Youtube Clone",
-    img: '/public/vidwebpng.png',
-  },
+  
 ]
   
 
@@ -43,12 +32,12 @@ const page = () => {
   return (
     
 
-    <div className='my-14 w-6xl flex flex-wrap flex-row justify-center'>
+    <div id='iterator' className='my-14 w-6xl flex flex-wrap flex-row justify-center'>
 
     {
       projects.map((project) => (
-        <div  className=' m-6 p-4 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
-        <div key={project.key}>
+        <div key={project.id} className=' m-6 p-4 h-96 w-80 backdrop-blur-xl hover:scale-110 hover:ease-in duration-300 rounded-sm border-4  border-slate-50 hover:border-slate-400 hover:shadow-black hover:shadow-2xl'>
+        <div >
         <Image height={500} width={500} src={require('/public/vidwebpng.png')}/>
         </div>
         <div className='overflow-y-scroll h-3/5 scrollbar-thin scrollbar-rounded-full scrollbar-thumb-purple-600  scrollbar-track-gray-300 flex flex-col text-hereis pr-2 py-4'>
