455a2c04ea64421fc5440d9e496c5f0f40dfaf7d
bharat2002 <bpaliwal70@gmail.come>
2023-07-30 12:10:12 +0530

    changesincontact

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <form className="container h-auto w-1/2 m-14 flex flex-col text-slate-50 justify-between items-start">
-      <h1 className='text-3xl text-center pb-6 text-slate-50'>Get in touch</h1>
+    <div>
+    <form className=" h-screen m-14 flex flex-col text-slate-50 justify-start items-center">
+      <h1 className='text-3xl text-center pb-6 text-purple-600'>Get in touch</h1>
       <div className="flex flex-col justify-center p-2 ">
         <label htmlFor="frm-email">Email</label>
         <input
-         className='p-2 rounded-lg text-slate-90'
+         className='p-2 rounded-lg bg-slate-800 border-2 text-slate-50'
           id="frm-email"
           type="email"
           name="email"
@@ -18,9 +19,9 @@ export default function Home() {
       <div className="flex flex-col p-2">
         <label htmlFor="frm-phone">Phone</label>
         <input
-         className='p-2 rounded-lg'
+         className='p-2 rounded-lg bg-slate-800 border-2'
           id="frm-phone"
-          type="text"
+          type="tel"
           name="phone"
           autoComplete="tel"
           required
@@ -30,7 +31,7 @@ export default function Home() {
         <div className='flex flex-col'>
           <label htmlFor="frm-first">First Name</label>
           <input
-           className='p-2 rounded-lg'
+           className='p-2 rounded-lg bg-slate-800 border-2 '
             id="frm-first"
             type="text"
             name="first"
@@ -41,7 +42,7 @@ export default function Home() {
         <div className='flex flex-col pt-2'>
           <label htmlFor="frm-last">Last Name</label>
           <input  
-          className='p-2 rounded-lg'
+          className='p-2 rounded-lg bg-slate-800 border-2 text-slate-50'
             id="frm-last"
             type="text"
             name="last"
@@ -49,14 +50,16 @@ export default function Home() {
             required
           />
         </div>
-      </div>
-      <div className=" w-3/4 flex flex-col p-2">
+        <div className="flex flex-col justify-center items-start">
         <label htmlFor="frm-message">Message</label>
-        <textarea  className=' w-full p-2 rounded-lg' id="frm-message" rows="6" name="message"></textarea>
+        <textarea  className=' w-full bg-slate-800 border-2 text-slate-50 p-2 rounded-lg' id="frm-message" rows="6" name="message"></textarea>
       </div>
-      <div className="flex flex-col justify-center items-center">
-        <button type="submit">Submit</button>
+      </div>
+     
+      <div className="flex w-full mt-4 justify-center items-center">
+        <button className='border-2 hover:bg-purple-600  p-2 rounded-lg text-slate-50' type="submit">Submit</button>
       </div>
     </form>
+    </div>
   );
 }
